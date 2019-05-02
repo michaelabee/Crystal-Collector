0d34028896d984c5caff18ee0f95b66f36a07727
Michaela Banczak <m.a.banczak@gmail.com>
2019-05-02 18:06:23 -0500

    added click events to each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,22 +6,21 @@ $('#target').text(targetNumber);
 var userTotal = 0;
 $('#total').text(userTotal);
 
-var blueCrystal = Math.floor(Math.random()*18+1);
+var blueCrystal = Math.floor(Math.random()*12+1);
 console.log(blueCrystal);
-$(blueCrystal).addClass("#blue");
+$("#blue").add;
 
-var orangeCrystal = Math.floor(Math.random()*18+1);
+var orangeCrystal = Math.floor(Math.random()*12+1);
 console.log(orangeCrystal);
 $(orangeCrystal).addClass("#orange");
 
-var purpleCrystal = Math.floor(Math.random()*18+1);
+var purpleCrystal = Math.floor(Math.random()*12+1);
 console.log(purpleCrystal);
 $(purpleCrystal).addClass("purple");
 
-var whiteCrystal = Math.floor(Math.random()*18+1);
+var whiteCrystal = Math.floor(Math.random()*12+1);
 console.log(whiteCrystal);
-
-//$(whiteCrystal).addClass("#white"); (is this how I assign the html class tag to this variable)?
+$(whiteCrystal).addClass("#white");
 
 
 var wins = 0;
@@ -30,13 +29,41 @@ var losses = 0;
 $("#losses").text(losses);
 
 
+$("#blue").click(function() {
+    console.log("clicked");
+    userTotal += blueCrystal;
+    $("#total").text(userTotal);
+});
+
+$("#orange").click(function() {
+    console.log("clicked");
+    userTotal += orangeCrystal;
+    $("#total").text(userTotal);
+});
+
+$("#purple").click(function() {
+    console.log("clicked");
+    userTotal += purpleCrystal;
+    $('#total').text(userTotal);
+});
+
+$("#white").click(function() {
+    console.log("clicked");
+    userTotal += whiteCrystal;
+    $('#total').text(userTotal);
+});
+
+
 if (userTotal === targetNumber){
     wins ++;
+    $("wins").text(wins);
     //run reset function here
 }
 else if (userTotal > targetNumber) {
     losses ++;
+    $('#losses').text(losses);
     //runreset function here
 }
 
 })
+
