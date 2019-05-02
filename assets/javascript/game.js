0a5137c333f74cd4299f28371b7298525c4b4913
Michaela Banczak <m.a.banczak@gmail.com>
2019-05-01 20:52:49 -0500

    generates numbers for each crystal, defines variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,42 @@
 $( document ).ready(function(){
 
 var targetNumber = Math.floor(Math.random()*101+19);
-console.log (targetNumber);
 $('#target').text(targetNumber);
 
+var userTotal = 0;
+$('#total').text(userTotal);
+
+var blueCrystal = Math.floor(Math.random()*18+1);
+console.log(blueCrystal);
+$(blueCrystal).addClass("#blue");
+
+var orangeCrystal = Math.floor(Math.random()*18+1);
+console.log(orangeCrystal);
+$(orangeCrystal).addClass("#orange");
+
+var purpleCrystal = Math.floor(Math.random()*18+1);
+console.log(purpleCrystal);
+$(purpleCrystal).addClass("purple");
+
+var whiteCrystal = Math.floor(Math.random()*18+1);
+console.log(whiteCrystal);
+
+//$(whiteCrystal).addClass("#white"); (is this how I assign the html class tag to this variable)?
 
 
+var wins = 0;
+$("#wins").text(wins);
+var losses = 0;
+$("#losses").text(losses);
 
 
-
+if (userTotal === targetNumber){
+    wins ++;
+    //run reset function here
+}
+else if (userTotal > targetNumber) {
+    losses ++;
+    //runreset function here
+}
 
 })
